e5c802d715e855b8254d6e38ecb0f3c8ec283a6f
Joe Simpson <josephsimpson94@googlemail.com>
2023-11-14 15:58:18 +0000

    feature: basic landing info added

--- src/components/landinginfo/LandingInfo.js
+++ src/components/landinginfo/LandingInfo.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const LandingInfo = () => {
-  return <div>LandingInfo</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Joe</h1>
+      <p>
+        I'm a junior full stack developer based out of Ellesmere Port, Cheshire.
+        Thank you for taking the time to visit my website!
+      </p>
+    </div>
+  );
 };
 
 export default LandingInfo;
